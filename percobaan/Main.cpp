e82f406b9210fd1add3199cab4f3d444081f86e2
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-02-29 19:28:04 +0700

    pembaruan

--- percobaan/Main.cpp
+++ percobaan/Main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+
+void Output(string a, char b, int c) {
+	cout << "Nama karyawan: " << a<< endl;
+	cout << "Golongan Karyawan: " << b << endl;
+	cout << "Total Gaji Karyawan:  " << c << endl;
+}
+
 
 int main()
 {
@@ -29,49 +38,72 @@ int main()
 	int upah_perjam;
 	int total_upah;
 
-	cout << "Nama karyawan: " << endl;
-	cin >> Nama;
+	cout << "Nama karyawan: ";
+	getline(cin, Nama);
 
-	cout << "Golongan karyawan: " << endl;
+	cout << "Golongan karyawan: ";
 	cin >> Golongan;
 
-	cout << "Jam kerja: " << endl;
+	cout << "Jam kerja: ";
 	cin >> jam_kerja;
 
 
-	switch (Golongan) {
+	/*switch (Golongan) {
 	case 'A':
-		upah_perjam = 5.000;
+		upah_perjam = 5000;
 		break;
-	case 'B:':
-		upah_perjam = 7.000;
+	case 'B':
+		upah_perjam = 7000;
 		break;
 	case 'C':
-		upah_perjam = 8.000;
+		upah_perjam = 8000;
 		break;
 	case 'D':
-		upah_perjam = 10.000;
+		upah_perjam = 10000;
 		break;
 	default:
 		upah_perjam = 0;
 		break;
-	}
+	}*/
 	
+	if ((Golongan == 'A') || ( Golongan == 'a')) {
+		upah_perjam = 5000;
+	}
+	else if ((Golongan == 'B') || (Golongan == 'b')) {
+		upah_perjam = 7000;
+	}
+	else if ((Golongan == 'C') || (Golongan == 'c')) {
+		upah_perjam = 8000;
+	}
+	else if ((Golongan == 'D') || (Golongan == 'd')) {
+		upah_perjam = 10000;
+	}
+	else {
+		cout << "Golongan Tidak Terdapat Dalam DataBase" << endl;
+	}
 	upah = jam_kerja * upah_perjam;
 
 
 	if (jam_kerja > 48) {
-		uang_lembur = (jam_kerja - 48) * 4.000;
+		uang_lembur = (jam_kerja - 48) * 4000;
 	}
 	else
 	{
 		uang_lembur = 0;
 	}
 
-	total_upah = upah * uang_lembur;
+	total_upah = upah + uang_lembur;
 	
+	system("cls");
 	cout << "Nama: " << Nama << endl;
 	cout << "Gaji: " << total_upah << endl;
+
+
+	 /*string a = Nama;
+	 int b = Golongan;
+	 int c = total_upah;
+
+	 Output(a, b, c);*/
 
 
 	return 0;
